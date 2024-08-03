8c23b76f7f75e0f75fdf403666594551dc1c5260
Adrienn Mahler <ajanka23@gmail.com>
2024-08-03 09:52:56 +0200

    Initial commit

---
+++ src/MainHunderassen.java
@@ -0,0 +1,8 @@
+public class EnHunderassen {
+    public static void main(String[] args) {
+        Hundrassen dackel= Hundrassen.DACKEL;
+        Hundrassen collie= Hundrassen.COLLIE;
+        Hundrassen dogge= Hundrassen.DOGGE;
+        System.out.println(dackel + "\n" + collie+ "\n"+ dogge);
+    }
+}
